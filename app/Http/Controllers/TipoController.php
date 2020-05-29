96846674cb037bddc13158d47eb8f7837f31a97e
FREEWJCB <FREEWJCB@users.noreply.github.com>
2020-05-28 23:26:18 -0430

    Hay problema con la bd

--- app/Http/Controllers/TipoController.php
+++ app/Http/Controllers/TipoController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\tipo;
 class TipoController extends Controller
 {
     /**
@@ -14,6 +14,8 @@ class TipoController extends Controller
     public function index()
     {
         //
+        // $tipo = tipo::all();
+        // dd($tipo);
         return view('view.tipo');
     }
 
