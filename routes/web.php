e7af0e8984f1dbd6bae2c7ef33cf8175620d4ec0
FREEWJCB <FREEWJCB@users.noreply.github.com>
2020-05-31 21:41:15 -0430

    se agregaron las rutas de cargar y mostrar de todas las tablas

--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,22 @@ Route::get('/prueba', function () {
 });
 
 Route::resource('Tipo', 'TipoController')->except('show','edit');
-Route::resource('Marca', 'MarcaController')->except('show','edit');
-Route::resource('Modelo', 'ModeloController')->except('show','edit');
-Route::resource('Cosmetic', 'CosmeticController')->except('show','edit');
-Route::resource('Cosmetico', 'CosmeticoController')->except('show','edit');
 Route::POST('/Tipos', 'TipoController@cargar')->name('Tipo.cargar');
 Route::POST('/Tipo/rellenar', 'TipoController@mostrar')->name('Tipo.mostrar');
+
+Route::resource('Marca', 'MarcaController')->except('show','edit');
 Route::POST('/Marcas', 'MarcaController@cargar')->name('Marca.cargar');
+Route::POST('/Marca/rellenar', 'MarcaController@mostrar')->name('Marca.mostrar');
+
+Route::resource('Modelo', 'ModeloController')->except('show','edit');
 Route::POST('/Modelos', 'ModeloController@cargar')->name('Modelo.cargar');
+Route::POST('/Modelo/rellenar', 'ModeloController@mostrar')->name('Modelo.mostrar');
+
+
+Route::resource('Cosmetic', 'CosmeticController')->except('show','edit');
 Route::POST('/Cosmetics', 'CosmeticController@cargar')->name('Cosmetic.cargar');
+Route::POST('/Cosmetic/rellenar', 'CosmeticController@mostrar')->name('Cosmetic.mostrar');
+
+Route::resource('Cosmetico', 'CosmeticoController')->except('show','edit');
 Route::POST('/Cosmeticos', 'CosmeticoController@cargar')->name('Cosmetico.cargar');
-// Route::POST('/tipo_rellenar', 'TipoController@rellenar')->name('Tipo.rellenar');
+Route::POST('/Cosmetico/rellenar', 'CosmeticoController@mostrar')->name('Cosmetico.mostrar');
