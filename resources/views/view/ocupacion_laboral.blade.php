6582967188696de7a6ef23b8a19ad2b844496b75
Walter Caro <60450133+FREEWJCB@users.noreply.github.com>
2020-10-28 14:47:19 -0430

    se corrigio la vista de ocupacion_laboral y se agrego la eiqueta small

--- resources/views/view/ocupacion_laboral.blade.php
+++ resources/views/view/ocupacion_laboral.blade.php
@@ -7,8 +7,8 @@
 
 @section('busqueda')
 
-    <label for="bs_labor">Labor: &nbsp;&nbsp;&nbsp;</label>
-    <input name="bs_labor" id="bs_labor" onkeyup="mayuscula(this)" class="form-control mr-sm-2" type="text" placeholder="Buscar por labor" arialabel="Search" />
+    <label for="bs_labor"><b>Labor:</b> &nbsp;&nbsp;&nbsp;</label>
+    <input name="bs_labor" id="bs_labor" onkeyup="mayuscula(this)" onkeypress="return letra(event)" maxlength="255" class="form-control mr-sm-2" type="text" placeholder="Buscar por labor" arialabel="Search" />
 
 @endsection
 
@@ -27,9 +27,9 @@
 @section('tbody')
 
     @if ($num > 0)
-        @php($i=0)
+        @php $i=0 @endphp
         @foreach ($cons as $cons2)
-            @php($i++)
+            @php $i++ @endphp
             <tr>
                 <th scope="row"><center>{{ $i }}</center></th>
                 <td><center>{{ $cons2->labor }}</center></td>
@@ -46,9 +46,10 @@
 
 
     <div class="form-group">
-      <label for="labor">Labor</label>
-      <input type="text" onkeyup="mayuscula(this)" required class="form-control" id="labor" name="labor" />
+      <label for="labor"><b>Labor:</b></label>
+      <input type="text" onkeyup="mayuscula(this)" onkeypress="return letra(event)" maxlength="255" required class="form-control" id="labor" name="labor" />
       <input type="hidden" id="labor2" name="labor2" />
+      <small id="labor_e" style="color: red"></small>
     </div>
 
 
